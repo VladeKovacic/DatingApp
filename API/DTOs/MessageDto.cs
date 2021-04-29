627b4275452a5172b199391dbc82e7c0fb779305
Vlade Kovacic <kovacic.vlade@feg.eu>
2021-04-29 14:19:02 +0200

    Correction back

--- API/DTOs/MessageDto.cs
+++ API/DTOs/MessageDto.cs
@@ -7,14 +7,12 @@ namespace DatingApp.API.DTOs
     {
         public int Id { get; set; }
         public int SenderId { get; set; }
-
-        [JsonPropertyName("senderUsername")]
-        public string SenderUserName { get; set; }
+        public string SenderUsername { get; set; }
         public string SenderPhotoUrl { get; set; }
         public int RecipientId { get; set; }
         
         [JsonPropertyName("recipientUsername")]
-        public string RecipientUserName { get; set; }
+        public string RecipientUsername { get; set; }
         public string RecipientPhotoUrl { get; set; }
         public string Content { get; set; }
         public DateTime? DateRead { get; set; }
